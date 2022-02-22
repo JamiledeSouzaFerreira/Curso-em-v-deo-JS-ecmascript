faed5ca979d069d56352d402f3dd681eb3aa801a
Jamile de Souza Ferreira <jj2003.011@gmail.com>
2022-02-21 21:14:24 -0300

    Update index.js

--- Aula 12ex/ex14/index.js
+++ Aula 12ex/ex14/index.js
@@ -5,13 +5,13 @@ function manhã(){
     var hora = data.getHours()
     msg.innerHTML = (`Agora são ${hora} horas.`)
     if(hora < 12 && hora > 3){
-        msg.innerHTML += ('Bom Dia!')
+        msg.innerHTML += (' Bom Dia!')
         // img.src = ''
     } else if (hora > 12 && hora < 18){
         msg.innerHTML += (' Boa Tarde!')
         // img.src = ''
     } else {
-        msg.innerHTML +=('Boa Noite!')
+        msg.innerHTML +=(' Boa Noite!')
         // img.src = ''
     }
 }
