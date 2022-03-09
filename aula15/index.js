c16449fadd51d03a6bfc71b8ec33dac5d3da1570
Jamile de Souza Ferreira <jj2003.011@gmail.com>
2022-03-09 10:10:14 -0300

    aula 15

---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+let lista = [1, 5 , 8 , 3, 4, 9]
+
+/*for (let n = 0 ; n < lista.length ; n++){
+    console.log(`A posição ${n} representa o valor ${lista[n]}`)
+}
+lista.sort()
+for (let n in lista){
+    console.log(`A posição ${n} representa o valor ${lista[n]}`)
+}*/
+
+
